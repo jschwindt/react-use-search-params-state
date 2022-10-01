c307ee0baf1f67dce8731295b59cd93f19122376
Juan Schwindt <juan@schwindt.org>
2022-10-01 20:22:05 -0300

    Remove unnecessary *.js files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export * from './useSearchParamsState';
-=======
 export * from './useSearchParamsState'
->>>>>>> aa01c87 (Initial commit)
--- src/useSearchParamsState.ts
+++ src/useSearchParamsState.ts
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-
-import { useSearchParams } from "react-router-dom";
-
-export type SearchParamStateType = {
-  name: string;
-  type: "string" | "number" | "boolean";
-  defaultValue: string | number | boolean | Array<string | number | boolean> | null;
-  isArray?: boolean;
-};
-
-const paramToBool = (param: SearchParamStateType, searchParams: URLSearchParams) => {
-  const paramValue = searchParams.get(param.name);
-  // The only presence of a boolean param (value === '') is considered true
-  if (paramValue === "true" || paramValue === '1' || paramValue === '') return true;
-  if (paramValue === "false" || paramValue === '0') return false;
-
-  return param.defaultValue;
-=======
 import { useSearchParams } from 'react-router-dom'
 
 export type SearchParamStateType = {
@@ -33,62 +14,10 @@ const paramToBool = (param: SearchParamStateType, searchParams: URLSearchParams)
   if (paramValue === 'false' || paramValue === '0') return false
 
   return param.defaultValue
->>>>>>> aa01c87 (Initial commit)
 }
 
 const paramToValue = (param: SearchParamStateType, searchParams: URLSearchParams) => {
   if (param.isArray) {
-<<<<<<< HEAD
-    const paramValue = searchParams.getAll(param.name);
-    if (paramValue.length > 0) {
-      return param.type === "number" ? paramValue.map(value => Number(value)) : paramValue;
-    }
-  } else {
-    const paramValue = searchParams.get(param.name);
-    if (paramValue) {
-      return param.type === "number" ? Number(paramValue) : paramValue;
-    }
-  }
-  return param.defaultValue;;
-}
-
-const getValues = (paramsDefinition: SearchParamStateType[], searchParams: URLSearchParams) => {
-  const values: any = {};
-  paramsDefinition.forEach(param => {
-    if (param.type === "boolean") {
-      values[param.name] = paramToBool(param, searchParams);
-    } else {
-      values[param.name] = paramToValue(param, searchParams);
-    }
-  });
-  return values;
-}
-
-const getAllCurrentParams = (searchParams: URLSearchParams) => {
-  const allUrlParams: Record<string, any> = {};
-  for (const key of searchParams.keys()) {
-    const value = searchParams.getAll(key);
-    allUrlParams[key] = value;
-    if (value.length === 1) {
-      allUrlParams[key] = value[0];
-    }
-  }
-  return allUrlParams;
-};
-
-export const useSearchParamsState = (paramsDefinition: SearchParamStateType[]) => {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const values = getValues(paramsDefinition, searchParams);
-
-  const setValues = (newValues: Record<string, any>) => {
-    const currentParams = getAllCurrentParams(searchParams);
-    setSearchParams({ ...currentParams, ...newValues });
-  };
-
-  return [values, setValues] as const;
-};
-=======
     const paramValue = searchParams.getAll(param.name)
     if (paramValue.length > 0) {
       return param.type === 'number' ? paramValue.map((value) => Number(value)) : paramValue
@@ -143,4 +72,3 @@ export const useSearchParamsState = (paramsDefinition: SearchParamStateType[]) =
 
   return [values, setValues] as const
 }
->>>>>>> aa01c87 (Initial commit)
